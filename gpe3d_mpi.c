d457a441d30fe8b3632292f32b8fa303f1ea4a34
Shreyas Potnis <shreyaspotnis@gmail.com>
2014-05-13 16:22:10 -0400

    Added code to calculate expectation values.

--- gpe3d_mpi.c
+++ gpe3d_mpi.c
@@ -12,32 +12,60 @@
 #include "common.h"
 #include "configuration.h"
 
+
+/* struct declarations */
+typedef struct expectation_values {
+    double x_ex;
+    double y_ex;
+    double z_ex;
+    double x2_ex;
+    double y2_ex;
+    double z2_ex;
+} expectation_values;
+
+/* end of struct declarations */
+
 /* Globals */
 int rank, size;
 /* End of globals */
-
 
 /* Function declarations */
 int create_plans(configuration *cfg, fftw_plan *p_fwd, fftw_plan *p_bwd,
                  fftw_complex *psi_local);
 
-
 int create_1d_grids(double **x_grid, double **y_grid, double **z_grid,
                 configuration *cfg);
+
 int create_1d_k_grids(double **kx_grid, double **ky_grid, double **kz_grid,
                 configuration *cfg);
+
 int fill_k_grid(double *grid, int n_local, int n_global, int local_start,
                 double dk);
+
 double norm_squared(fftw_complex *vec, int length);
+
+double norm_4(fftw_complex *vec, int length);
+
 double potential_energy(configuration *cfg, double x, double y, double z);
+
+void init_psi(configuration *cfg, double *x_grid, double *y_grid,
+             double * z_grid, fftw_complex *psi_local);
 
 void x_unitary(configuration *cfg, double *x_grid, double *y_grid,
                double *z_grid, fftw_complex *psi_local,
                fftw_complex imag_pre_factor);
+
 void k_unitary(configuration *cfg, double *kx_grid, double *ky_grid,
                double *kz_grid, fftw_complex *psi_local,
                fftw_complex imag_pre_factor);
+
+expectation_values calculate_expectation_values(configuration *cfg,
+                                                double *x_grid, double *y_grid,
+                                                double *z_grid,
+                                                fftw_complex *psi_local);
 /* End of Function declarations */
+
+
 
 
 int main(int argc, char **argv) {
@@ -69,7 +97,6 @@ int main(int argc, char **argv) {
     fftw_complex *psi_local;
     fftw_plan p_fwd, p_bwd;
 
-    printf("Allocating memory for %d complex numbers", alloc_local);
     psi_local = fftw_alloc_complex(alloc_local);
     create_plans(&cfg, &p_fwd, &p_bwd, psi_local);
 
@@ -83,26 +110,7 @@ int main(int argc, char **argv) {
     create_1d_k_grids(&kx_grid, &ky_grid, &kz_grid, &cfg);
 
     // Initialize the wavefunction to the Thomas Fermi wavefunction
-
-    int current_index = 0;
-    double x_c, y_c, z_c;
-    int ix, iy, iz;
-    for(ix = 0; ix < cfg.Nx_local; ix++) {
-        x_c = x_grid[ix];
-        for(iy = 0; iy < cfg.Ny; iy++) {
-            y_c = y_grid[iy];
-            for(iz = 0; iz < cfg.Nz; iz++) {
-                z_c = z_grid[iz];
-                double pot_en = potential_energy(&cfg, x_c, y_c, z_c);
-                double psi2_val = (cfg.mu_theory - pot_en) / cfg.kappa;
-                if(psi2_val > 0.0)
-                    psi_local[current_index] = sqrt(psi2_val);
-                else
-                    psi_local[current_index] = 0.0;
-                current_index++;
-            }
-        }
-    }
+    init_psi(&cfg, x_grid, y_grid, z_grid, psi_local);
 
     fftw_complex imag_pre_factor;
     if(cfg.imag_time)
@@ -118,12 +126,6 @@ int main(int argc, char **argv) {
     /************************************/
     int j = 1, i = 0;
     for(i = 0; i < cfg.Nt; i++) {
-
-        int current_index = 0;
-        int ix, iy, iz;
-        double x_c, y_c, z_c;
-        double kx_c, ky_c, kz_c;
-
 
         // Do X-unitary
         x_unitary(&cfg, x_grid, y_grid, z_grid, psi_local, imag_pre_factor);
@@ -150,7 +152,7 @@ int main(int argc, char **argv) {
             double p2 = norm_squared(psi_local, alloc_local);  // get norm
             p2 *= cfg.dx * cfg.dy * cfg.dz;
             double p2_inv_sqrt = 1.0/sqrt(p2);
-            printf("p2: %f, 1/p2 %f\n", p2, p2_inv_sqrt);
+            // printf("p2: %f, 1/p2 %f\n", p2, p2_inv_sqrt);
             int i1;
             for(i1=0; i1 < alloc_local; i1++)
                 psi_local[i1] *= p2_inv_sqrt;
@@ -158,7 +160,30 @@ int main(int argc, char **argv) {
 
         MPI_Barrier(MPI_COMM_WORLD);
         if( (i + 1) % Nt_skip == 0) {
-            // printf("Time step %d, Store step %d\n", i, j);
+            double n4 = norm_4(psi_local, alloc_local);
+            expectation_values ex_vals;
+            ex_vals = calculate_expectation_values(&cfg, x_grid, y_grid,
+                                                   z_grid, psi_local);
+            if(rank == MASTER_RANK) {
+                double mfe = n4 * cfg.kappa * cfg.dx * cfg.dy * cfg.dz;
+                double pot_en = ex_vals.x2_ex +
+                                ex_vals.y2_ex * cfg.gamma_y * cfg.gamma_y +
+                                ex_vals.z2_ex * cfg.gamma_z * cfg.gamma_z;
+                pot_en *= 0.5;
+                double mu_ex = mfe + pot_en;
+
+                printf("\n*****************************\n");
+                printf("Time step %d, Store step %d\n", i, j);
+                print_double("MFE", mfe);
+                print_double("<X>", ex_vals.x_ex);
+                print_double("<Y>", ex_vals.y_ex);
+                print_double("<Z>", ex_vals.z_ex);
+                print_double("<X2>", ex_vals.x2_ex);
+                print_double("<Y2>", ex_vals.y2_ex);
+                print_double("<Z2>", ex_vals.z2_ex);
+                print_double("<U>", pot_en);
+                print_double("<mu>", mu_ex);
+            }
             j++;
         }
     }
@@ -258,10 +283,49 @@ double norm_squared(fftw_complex *vec, int length) {
 }
 
 
+double norm_4(fftw_complex *vec, int length) {
+    // find local sum
+    double sum_local, sum_global;
+    int j;
+    sum_local = 0.0;
+    for(j = 0; j < length; j++) {
+        double n2 = vec[j] * conj(vec[j]);
+        sum_local += n2 * n2;
+    }
+    MPI_Reduce(&sum_local, &sum_global, 1, MPI_DOUBLE, MPI_SUM, MASTER_RANK,
+                  MPI_COMM_WORLD);
+    return sum_global;
+}
+
+
 double potential_energy(configuration *cfg, double x, double y, double z) {
     // Returns the potential energy at the point (x, y, z)
     return 0.5 * (x * x + y * y * cfg->gamma_y * cfg->gamma_y
                   + z * z * cfg->gamma_z * cfg->gamma_z);
+}
+
+void init_psi(configuration *cfg, double *x_grid, double *y_grid,
+             double * z_grid, fftw_complex *psi_local) {
+
+    int current_index = 0;
+    double x_c, y_c, z_c;
+    int ix, iy, iz;
+    for(ix = 0; ix < cfg->Nx_local; ix++) {
+        x_c = x_grid[ix];
+        for(iy = 0; iy < cfg->Ny; iy++) {
+            y_c = y_grid[iy];
+            for(iz = 0; iz < cfg->Nz; iz++) {
+                z_c = z_grid[iz];
+                double pot_en = potential_energy(cfg, x_c, y_c, z_c);
+                double psi2_val = (cfg->mu_theory - pot_en) / cfg->kappa;
+                if(psi2_val > 0.0)
+                    psi_local[current_index] = sqrt(psi2_val);
+                else
+                    psi_local[current_index] = 0.0;
+                current_index++;
+            }
+        }
+    }
 }
 
 void x_unitary(configuration *cfg, double *x_grid, double *y_grid,
@@ -309,4 +373,53 @@ void k_unitary(configuration *cfg, double *kx_grid, double *ky_grid,
             }
         }
     }
+}
+
+expectation_values calculate_expectation_values(configuration *cfg,
+                                                double *x_grid, double *y_grid,
+                                                double *z_grid,
+                                                fftw_complex *psi_local) {
+    // Returns the expectation values on MASTER_RANK. Garbage for other
+    // processes
+    expectation_values ex_local;
+    expectation_values ex_global;
+    ex_local.x_ex = ex_local.y_ex = ex_local.z_ex = 0.0;
+    ex_local.x2_ex = ex_local.y2_ex = ex_local.z2_ex = 0.0;
+
+    int ix, iy, iz;
+    double x_c, y_c, z_c;
+    int current_index = 0;
+    for(ix = 0; ix < cfg->Nx_local; ix++) {
+        x_c = x_grid[ix];
+        for(iy = 0; iy < cfg->Ny; iy++) {
+            y_c = y_grid[iy];
+            for(iz = 0; iz < cfg->Nz; iz++) {
+                z_c = z_grid[iz];
+                fftw_complex psi_val = psi_local[current_index];
+                double p2 = psi_val * conj(psi_val);
+                ex_local.x_ex += p2 * x_c;
+                ex_local.y_ex+= p2 * y_c;
+                ex_local.z_ex+= p2 * z_c;
+
+                ex_local.x2_ex += p2 * x_c * x_c;
+                ex_local.y2_ex += p2 * y_c * y_c;
+                ex_local.z2_ex += p2 * z_c * z_c;
+
+                current_index++;
+            }
+        }
+    }
+    MPI_Reduce(&ex_local, &ex_global, 6, MPI_DOUBLE, MPI_SUM, MASTER_RANK,
+               MPI_COMM_WORLD);
+    double dV = cfg->dx * cfg->dy * cfg->dz;
+
+    ex_global.x_ex *= dV;
+    ex_global.y_ex *= dV;
+    ex_global.z_ex *= dV;
+
+    ex_global.x2_ex *= dV;
+    ex_global.y2_ex *= dV;
+    ex_global.z2_ex *= dV;
+
+    return ex_global;
 }
